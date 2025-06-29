f914bf806d8fb8fd23b8a903bbad490e42c4527b
Shawn10019O <zmi19ml@gmail.com>
2025-06-29 18:15:10 +0900

    chore: add minimal setup.py for tests

---
+++ setup.py
@@ -0,0 +1,8 @@
+# setup.py  (プロジェクトルート)
+from setuptools import setup, find_packages
+
+setup(
+    name="factchecker",
+    version="0.1.0",
+    packages=find_packages(),  # factchecker/ 以下を自動認識
+)
